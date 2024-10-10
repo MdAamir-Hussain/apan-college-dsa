c4f2808ddfce500e3399e5c36cce65fbe20bb8e1
MdAamir-Hussain <mdaamirhussain319@gmail.com>
2024-10-10 12:30:17 +0530

    insertion-sort

--- bubble.cpp
+++ bubble.cpp
@@ -32,6 +32,20 @@ void selectionSort(int arr[], int n){
     }
 }
 
+void insertionSort(int arr[], int n){
+
+    for(int i=1; i<n; i++){
+        int curr = arr[i]; 
+        int prev = i-1;
+
+        while(prev >=0 && arr[prev] > curr){
+            arr[prev+1] = arr[prev];
+            prev--;
+        }
+        arr[prev+1] = curr;
+    }
+}
+
 void printArray(int arr[], int n ){
 
     for(int i=0; i<n; i++){
@@ -47,8 +61,11 @@ int main(){
 
     bubbleSort(arr, n);
     printArray(arr, n);
-    
+
     selectionSort(arr, n);
+    printArray(arr, n);
+
+    insertionSort(arr, n);
     printArray(arr, n);
 
     return 0;
