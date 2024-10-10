8fff23cc0f47dc7be7913883258ec83b5f10afb7
MdAamir-Hussain <mdaamirhussain319@gmail.com>
2024-10-10 12:03:42 +0530

    bubble-sort-optimization

--- bubble.cpp
+++ bubble.cpp
@@ -3,12 +3,18 @@ using namespace std;
 
 void bubbleSort(int arr[], int n){
     for(int i=0; i<n-1; i++){
+        bool isSwap = false;
 
         for(int j=0; j<n-i-1; j++){
-
             if(arr[j] > arr[j+1]){
                 swap(arr[j], arr[j+1]);
+                isSwap = true;
             }
+        }
+
+        if(!isSwap){
+            //arry is already sorted
+            return;
         }
     }
 }
