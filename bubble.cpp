ac352348396077391baf5d76b9cb8bbd58e99b7b
MdAamir-Hussain <mdaamirhussain319@gmail.com>
2024-10-10 12:13:27 +0530

    selection-sort

--- bubble.cpp
+++ bubble.cpp
@@ -19,6 +19,19 @@ void bubbleSort(int arr[], int n){
     }
 }
 
+void selectionSort(int arr[], int n){
+    for(int i=0; i<n-1; i++){
+
+        int smallestIdx = i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j] < arr[smallestIdx]){
+                smallestIdx = j;
+            }
+        }
+        swap(arr[i], arr[smallestIdx]);
+    }
+}
+
 void printArray(int arr[], int n ){
 
     for(int i=0; i<n; i++){
@@ -33,6 +46,9 @@ int main(){
     int n = 7;
 
     bubbleSort(arr, n);
+    printArray(arr, n);
+    
+    selectionSort(arr, n);
     printArray(arr, n);
 
     return 0;
